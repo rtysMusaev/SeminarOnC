7af611ff09b541d55f6b4cf49fc859ae58a0d040
Maksim <maksim.musaev2015@mail.ru>
2023-03-01 16:43:26 +0900

    решил Task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -7,3 +7,15 @@
 // 25, 5 -> да
 // 8,9 -> нет
 
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Sqrt(number1, number2) || Sqrt(number2, number1) ? "Да" : "Нет");
+
+bool Sqrt(int num1, int num2)
+{
+     return num1 * num1 == num2;
+}
